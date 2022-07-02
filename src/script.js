92928618338bc36fa290e99195fa1456e2902fa6
ivanniesala <Ivannie.c@gmail.com>
2022-07-02 19:02:37 -0400

    added welcome js

---
+++ src/script.js
@@ -0,0 +1,12 @@
+
+/*function welcome() {
+  let askName = prompt("What is your name?");
+  askName = askName.trim();
+  let heading = document.querySelector("#welcome");
+  if (askName === "") {
+    heading.innerHTML = "Hi! My name is, ";
+  } else {
+    heading.innerHTML = "Welcome, " + askName + "! My name is";
+  }
+}
+welcome();
